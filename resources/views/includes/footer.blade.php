84b78057a0141925a90383240f25af10bd649044
Taufikmyname <Taufiktrinugroho1@gmail.com>
2025-01-21 21:41:55 +0700

    Initial commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,43 @@
+
+<footer class="bg-gray-900 text-white">
+    <div class="container mx-auto pt-3">
+        <h5 class="text-center mb-3" data-aos="fade-up" data-aos-delay="{{ $incrementCategory = 100 }}">
+            It's Really That <br />
+            Easy to Win the Game
+        </h5>
+        <div class="flex flex-wrap justify-center gap-4">
+            <div class="w-full lg:w-1/3" data-aos="fade-up" data-aos-delay="{{ $incrementCategory = 200 }}">
+                <div class="bg-gray-800 p-3 rounded-lg shadow-md">
+                    <p class="text-lg font-semibold mb-2">1. Start</p>
+                    <p class="text-lg mb-0">Choose a game <br /> that you want to top-up</p>
+                </div>
+            </div>
+            <div class="w-full lg:w-1/3" data-aos="fade-up" data-aos-delay="{{ $incrementCategory = 300 }}">
+                <div class="bg-gray-800 p-3 rounded-lg shadow-md">
+                    <p class="text-lg font-semibold mb-2">2. Fill Up</p>
+                    <p class="text-lg mb-0">Top-up according <br /> to available products</p>
+                </div>
+            </div>
+            <div class="w-full lg:w-1/3" data-aos="fade-up" data-aos-delay="{{ $incrementCategory = 400 }}">
+                <div class="bg-gray-800 p-3 rounded-lg shadow-md">
+                    <p class="text-lg font-semibold mb-2">3. Be a Winner</p>
+                    <p class="text-lg mb-0">Ready to use <br /> to improve your game</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mx-auto text-center pt-4">
+        <p class="text-lg font-semibold">Get in touch</p>
+        <a class="mr-2 inline-block" href="mailto:easytopup.str@gmail.com">
+            <img src="/images/icons-gmail.svg" alt="Gmail" class="inline-block">
+        </a>
+        <a class="inline-block" href="https://www.instagram.com/easytopup.store/">
+            <img src="/images/icons-instagram.svg" alt="Instagram" class="inline-block">
+        </a>
+    </div>
+    <div class="col-12 text-center">
+        <p class="pt-4 pb-2">
+            © 2021 EASYTOPUP. All Rights Reserved.
+        </p>
+    </div>
+</footer>
