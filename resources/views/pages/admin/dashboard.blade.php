ce32d7c56409ba1188d7a3b8816cf562340158de
Taufikmyname <Taufiktrinugroho1@gmail.com>
2025-02-02 14:25:03 +0700

    page product create

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -6,7 +6,7 @@ Voucher Wave Dashboard
 
 @section('content')
     <!-- Section Content-->
-          <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+          <div class="section-content section-dashboard-home" data-aos="fade-up">
             <div class="container-fluid">
               <div class="dashboard-heading">
                 <h2 class="dashboard-title">Admin Dashboard</h2>
--- resources/views/pages/admin/product/create.blade.php
+++ resources/views/pages/admin/product/create.blade.php
@@ -8,10 +8,8 @@
     <!-- Section Content-->
           <div class="section-content section-dashboard-home" data-aos="fade-up">
             <div class="container-fluid">
-              <div class="dashboard-heading">
-                <h2 class="dashboard-title">Product</h2>
-                <p class="dashboard-subtitle">Create New Product</p>
-              </div>
+                <h2 class=" text-xl font-bold text-gray-900 dark:text-white">My Account</h2>
+                <p class="mb-4 text-base text-gray-900 dark:text-white">Update your current profile</p>
               <div class="dashboard-content">
                 <div class="row">
                     <div class="col-md-12">
@@ -28,16 +26,16 @@
                             <div class="card-body">
                                 <form action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data">
                                     @csrf
-                                    <div class="row">
-                                        <div class="col-md-12">
+                                    <div class="row ">
+                                        <div class="col-md-12 ">
                                             <div class="form-group">
-                                                <label>Product Name</label>
+                                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Name</label>
                                                 <input type="text" name="name" class="form-control" required>
                                             </div>
                                         </div>
                                         <div class="col-md-12">
                                             <div class="form-group">
-                                                <label>Product Category</label>
+                                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Category</label>
                                                 <select name="categories_id" class="form-control">
                                                     @foreach ($categories as $category)
                                                         <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -47,13 +45,13 @@
                                         </div>
                                         <div class="col-md-12">
                                             <div class="form-group">
-                                                <label>Product Price</label>
+                                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Price</label>
                                                 <input type="number" name="price" class="form-control" required>
                                             </div>
                                         </div>
                                         <div class="col-md-12">
                                             <div class="form-group">
-                                                <label>Product Description</label>
+                                                <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
                                                 <textarea name="description" id="editor"></textarea>
                                             </div>
                                         </div>
