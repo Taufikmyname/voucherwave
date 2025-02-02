41922cb10dd70ced033f1f7bf911fad446eaa512
Taufikmyname <Taufiktrinugroho1@gmail.com>
2025-02-02 13:54:16 +0700

    admin side bar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,24 +20,72 @@
     <div class="page-dashboard">
       <div class="d-flex" id="wrapper" data-aos="fade-right">
         <!-- Sidebar -->
-        <div class="border-right" id="sidebar-wrapper">
-          <div class="sidebar-heading text-center">
-            <img src="/images/dashboard-logo.svg" alt="" class="my-4" />
+        <aside id="separator-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+          <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+            <a href="{{ route('home') }}" class="grid h-10 w-32 place-content-center rounded-lg bg-gray-100 text-base text-gray-600 ">
+              Voucher Wave
+            </a>
+            <ul class="space-y-2 font-medium">
+                <li>
+                  <a href="{{ route('admin-dashboard') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ (request()->is('admin/dashboard')) ? 'bg-sky-400 text-dark' : '' }}">
+                      <span class="ms-3">Dashboard</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="{{ route('product.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ (request()->is('admin/product*')) ? 'bg-sky-400 text-dark' : '' }}">
+                      <span class="flex-1 ms-3 whitespace-nowrap">Product</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="{{ route('product-gallery.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ (request()->is('admin/product-gallery*')) ? 'bg-sky-400 text-dark' : '' }}">
+                      <span class="flex-1 ms-3 whitespace-nowrap">Galleries</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="{{ route('category.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ (request()->is('admin/category*')) ? 'bg-sky-400 text-dark' : '' }}">
+                      <span class="flex-1 ms-3 whitespace-nowrap">Categories</span>
+                  </a>
+                </li>
+                <li class="">
+                  <a href="{{ route('banner.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ (request()->is('admin/banner*')) ? 'bg-sky-400 text-dark' : '' }}">
+                      <span class="flex-1 ms-3 whitespace-nowrap ">Banner</span>
+                    </a>
+                </li>
+                <li class="">
+                  <a href="{{ route('transaction.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ (request()->is('admin/transaction*','admin/transaction/details*')) ? 'bg-sky-400 text-dark' : '' }}">
+                      <span class="flex-1 ms-3 whitespace-nowrap ">Transaction</span>
+                    </a>
+                </li>
+                <li class="">
+                  <a href="{{ route('user.index') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group {{ (request()->is('admin/user*')) ? 'bg-sky-400 text-dark' : '' }}">
+                      <span class="flex-1 ms-3 whitespace-nowrap ">User</span>
+                    </a>
+                </li>
+                <li>
+                  <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="size-5 opacity-75"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                    />
+                  </svg>
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                      </form>
+                      <span class="flex-1 ms-3 whitespace-nowrap">Logout</span>
+                  </a>
+                </li>
+            </ul>
           </div>
-          <div class="list-group list-group-flush">
-            <a href="{{ route('admin-dashboard') }}" class="list-group-item list-group-item-action {{ (request()->is('admin/dashboard')) ? 'active' : '' }}">Dashboard</a>
-            <a href="{{ route('product.index') }}" class="list-group-item list-group-item-action {{ (request()->is('admin/product')) ? 'active' : '' }}">Products</a>
-            <a href="{{ route('product-gallery.index') }}" class="list-group-item list-group-item-action {{ (request()->is('admin/product-gallery*')) ? 'active' : '' }}">Galleries</a>
-            <a href="{{ route('category.index') }}" class="list-group-item list-group-item-action {{ (request()->is('admin/category*')) ? 'active' : '' }}">Categories</a>
-             <a href="{{ route('banner.index') }}" class="list-group-item list-group-item-action {{ (request()->is('admin/banner*')) ? 'active' : '' }}">Banners</a>
-            <a href="{{ route('transaction.index') }}" class="list-group-item list-group-item-action {{ (request()->is('admin/transaction*','admin/transaction/details*')) ? 'active' : '' }}">Transactions</a>
-            <a href="{{ route('user.index') }}" class="list-group-item list-group-item-action {{ (request()->is('admin/user*')) ? 'active' : '' }}">Users</a>
-            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="list-group-item list-group-item-action">Sign Out</a>
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  @csrf
-            </form>
-          </div>
-        </div>
+        </aside>
 
         <!-- Page Content-->
         <div id="page-content-wrapper">
